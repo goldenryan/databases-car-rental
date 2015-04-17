b0df9130350a2aa13289a91bd0a81f5d4712f6d3
Ryan Golden <goldenrp@clarkson.edu>
2015-04-16 21:59:50 -0400

    intial commit

---
+++ hello.php
@@ -0,0 +1,26 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "";
+$dbname = "rectum";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM renter;";
+$result = $conn->query($sql);
+
+print_r($result->fetch_assoc());
+$conn->close();
+?>
+ </body>
+</html>
