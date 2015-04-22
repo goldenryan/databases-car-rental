f2b3f7242e36fe3b3c7ac5f34c7b37654aea42fe
Ryan Golden <goldenrp@clarkson.edu>
2015-04-21 20:41:37 -0400

    header updates to database creation

--- header.php
+++ header.php
@@ -57,14 +57,60 @@ if ($conn->query("create table if not exists takes(renter_id int, trip_id int);"
 if ($conn->query("create table if not exists to_loc (location_id int, trip_id int);") === FALSE)  {
 	printf("Failed to create table to_loc");
 }
-//$conn->query("ALTER TABLE to_loc ADD UNIQUE (trip_id);");
 if ($conn->query("create table if not exists with_car (vin varchar(50),trip_id int);") === FALSE)  {
 	printf("Failed to create table with_car");
 }
-//$conn->query("ALTER TABLE to_loc ADD UNIQUE (trip_id);");
 if ($conn->query("create table if not exists is_at (vin varchar(50),location_id int);") === FALSE)  {
 	printf("Failed to create table is_at");
 }
+//alters after here
+if ($conn->query("ALTER TABLE location ADD PRIMARY KEY (location_id);") === FALSE)  {
+	printf("Failed to create primary key in location");
+}
+if ($conn->query("ALTER TABLE location MODIFY COLUMN location_id INT NOT NULL AUTO_INCREMENT;") === FALSE)  {
+	printf("Failed to auto increment primary key in location");
+}
+if ($conn->query("ALTER TABLE cars ADD PRIMARY KEY (vin);") === FALSE)  {
+	printf("Failed to create primary key in cars");
+}
+if ($conn->query("ALTER TABLE renter ADD PRIMARY KEY (renter_id);") === FALSE)  {
+	printf("Failed to create primary key in renter");
+}
+if ($conn->query("ALTER TABLE renter MODIFY COLUMN renter_id INT NOT NULL AUTO_INCREMENT;") === FALSE)  {
+	printf("Failed to auto increment primary key in renter");
+}
+if ($conn->query("ALTER TABLE trip ADD PRIMARY KEY (trip_id);") === FALSE)  {
+	printf("Failed to create primary key in trip");
+}
+if ($conn->query("ALTER TABLE trip MODIFY COLUMN trip_id INT NOT NULL AUTO_INCREMENT;") === FALSE)  {
+	printf("Failed to auto increment primary key in trip");
+}
+//foreign keys after here
+if ($conn->query("ALTER TABLE is_at ADD CONSTRAINT loc_id FOREIGN KEY (location_id) REFERENCES location(location_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add location foreign key in is_at");
+}
+if ($conn->query("ALTER TABLE is_at  ADD CONSTRAINT fk_vin FOREIGN KEY (vin) REFERENCES cars(vin) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add car foreign key in is_at");
+}
+if ($conn->query("ALTER TABLE with_car ADD CONSTRAINT fk_vin_wc FOREIGN KEY (vin) REFERENCES cars(vin) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add car foreign key in with_car");
+}
+if ($conn->query("ALTER TABLE with_car ADD CONSTRAINT fk_tid_wc FOREIGN KEY (trip_id) REFERENCES trip(trip_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add trip foreign key in with_car");
+}
+if ($conn->query("ALTER TABLE to_loc ADD CONSTRAINT fk_lid_tl FOREIGN KEY (location_id) REFERENCES location(location_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add location foreign key in to_loc");
+}
+if ($conn->query("ALTER TABLE to_loc ADD CONSTRAINT fk_tid_tl FOREIGN KEY (trip_id) REFERENCES trip(trip_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add trip foreign key in to_loc");
+}
+if ($conn->query("ALTER TABLE takes ADD CONSTRAINT fk_rid_t FOREIGN KEY (renter_id) REFERENCES renter(renter_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add renter foreign key in takes");
+}
+if ($conn->query("ALTER TABLE takes ADD CONSTRAINT fk_tid_t FOREIGN KEY (trip_id) REFERENCES trip(trip_id) ON DELETE CASCADE;") === FALSE)  {
+	printf("Failed to add trip foreign key in takes");
+}
+
 //This makes it so the is_at table cannot hold duplicate entries of vin. Saying a car is a a location multiple times is a waste.
 //$conn->query("ALTER TABLE is_at ADD UNIQUE (vin);");
 
